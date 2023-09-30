2847827da0337d3b4c2625977b890c28cf2f36d5
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-10-01 02:16:32 +0700

    Update __main__.py

--- Amang/__main__.py
+++ Amang/__main__.py
@@ -70,7 +70,9 @@ async def start_bot():
 
 if __name__ == "__main__":
     try:
-        loop.run_until_complete(start_bot())
+        get_event_loop().run_until_complete(start_bot())
+        app.loop.run_forever()
+        # loop.run_until_complete(start_bot())
     except KeyboardInterrupt:
         pass
     except Exception:
@@ -78,4 +80,6 @@ if __name__ == "__main__":
         LOGGER.info(err)
     finally:
         loop.stop()
-        LOGGER.info ("------------------------ Stopped Services ------------------------")
+        LOGGER.info(
+            "------------------------ Stopped Services ------------------------"
+        )
