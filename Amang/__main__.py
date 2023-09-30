0a280515ce35d40a9498aa7461c046b223f2c5a5
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-10-01 01:13:33 +0700

    Update __main__.py

--- Amang/__main__.py
+++ Amang/__main__.py
@@ -78,4 +78,4 @@ if __name__ == "__main__":
         LOGGER.info(err)
     finally:
         loop.stop()
-        LOGGER ("------------------------ Stopped Services ------------------------")
+        LOGGER.info ("------------------------ Stopped Services ------------------------")
